4bf665b8afc9dd4966def72b90d39518bb83d98e
liweiyap <liweiyap@gmail.com>
2020-08-07 23:13:53 +0100

    Change settings button type from MenuButton to Button. Let it open a settings window.

--- text-popover-macOS/SettingsButton.swift
+++ text-popover-macOS/SettingsButton.swift
@@ -9,23 +9,103 @@
 import SwiftUI
 import Combine
 
+struct TimeMenuItemButton: View
+{
+    @Binding var timer: Publishers.Autoconnect<Timer.TimerPublisher>
+    let myWindow: NSWindow?
+    let name: String
+    let value: Double
+    
+    var body: some View
+    {
+        Button(name)
+        {
+            self.timer = Timer.publish(every: self.value, on: .main, in: .common).autoconnect()
+            self.myWindow!.close()
+        }
+    }
+}
+
+struct IntervalSettingsView: View
+{
+    @Binding var timer: Publishers.Autoconnect<Timer.TimerPublisher>
+    let myWindow: NSWindow?
+    
+    var body: some View
+    {
+        MenuButton("Minuten")
+        {
+            TimeMenuItemButton(timer: self.$timer, myWindow: myWindow,
+                               name: "5 Minuten", value: 5.0)
+            
+            Divider()
+            
+            TimeMenuItemButton(timer: self.$timer, myWindow: myWindow,
+                               name: "10 Minuten", value: 10.0 * Double(Int.secondsPerMinute))
+            
+            TimeMenuItemButton(timer: self.$timer, myWindow: myWindow,
+                               name: "15 Minuten", value: 15.0 * Double(Int.secondsPerMinute))
+            
+            Divider()
+            
+            TimeMenuItemButton(timer: self.$timer, myWindow: myWindow,
+                               name: "20 Minuten", value: 20.0 * Double(Int.secondsPerMinute))
+            
+            TimeMenuItemButton(timer: self.$timer, myWindow: myWindow,
+                               name: "25 Minuten", value: 25.0 * Double(Int.secondsPerMinute))
+            
+            Divider()
+            
+            TimeMenuItemButton(timer: self.$timer, myWindow: myWindow,
+                               name: "30 Minuten", value: 30.0 * Double(Int.secondsPerMinute))
+            
+            TimeMenuItemButton(timer: self.$timer, myWindow: myWindow,
+                               name: "35 Minuten", value: 35.0 * Double(Int.secondsPerMinute))
+        }
+        .frame(width: 100.0)
+    }
+}
+
+struct AllSettingsView: View
+{
+    @Binding var timer: Publishers.Autoconnect<Timer.TimerPublisher>
+    let myWindow: NSWindow?
+    
+    var body: some View
+    {
+        TabView
+        {
+            IntervalSettingsView(timer: self.$timer, myWindow: myWindow)
+            .tabItem
+            {
+                Text("Intervall")
+            }
+        }
+    }
+}
+
 struct SettingsButton: View
 {
     @Binding var timer: Publishers.Autoconnect<Timer.TimerPublisher>
     
     var body: some View
     {
-        MenuButton(">")
+        Button("⚙")
         {
-            Button("5 Sekunden", action: {
-                self.timer = Timer.publish(every: 5.0, on: .main, in: .common).autoconnect()
-            })
+            NSApplication.shared.keyWindow?.close()
             
-            Button("24 Stunden", action: {
-                self.timer = Timer.publish(every: TimeInterval(24 * Int.secondsPerHour), on: .main, in: .common).autoconnect()
-            })
+            let window = NSWindow(
+                contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+                styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+                backing: .buffered,
+                defer: false
+            )
+            window.center()
+            window.setFrameAutosaveName("Settings")
+            window.contentView = NSHostingView(rootView: AllSettingsView(timer: self.$timer, myWindow: window))
+//            window.makeKeyAndOrderFront(nil)
+            window.orderFront(nil)
+            window.isReleasedWhenClosed = false
         }
-        .frame(width: 10.0)
-        .menuButtonStyle(BorderlessButtonMenuButtonStyle())
     }
 }
